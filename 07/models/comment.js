4ec3dd0a77a7350ecb5a593f98d22e40a3910496
tiara79 <loverjj2@naver.com>
2025-06-25 15:10:32 +0900

    relation

---
+++ 07/models/comment.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+  const Comment = sequelize.define("Comment", {
+    content: DataTypes.TEXT,
+  }, {
+    tableName: "comments"
+  });
+
+  Comment.associate = function(models) {
+    Comment.belongsTo(models.Post, {
+      foreignKey: "postId",
+      as: "post",
+    });
+
+    Comment.belongsTo(models.User, {
+      foreignKey: "userId",
+      as: "author",
+    });
+  };
+
+  return Comment;
+};
---
+++ 07/models/post.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+  const Post = sequelize.define(
+    "Post",
+    {
+      title: DataTypes.STRING,
+      conent: DataTypes.STRING,
+      fileName: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+    },
+    {
+      tableName: "posts",
+    }
+  );
+  Post.associate = function (models) {
+    Post.belongsTo(models.User, {
+      foreignKey: "authorId",
+      as: "author",
+    });
+    Post.hasMany(models.Comment, {
+      foreignKey: "postId",
+      as: "comments",
+    });
+  };
+  return Post;
+};
