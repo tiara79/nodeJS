533d89c7c64fbf55b177be89751b43f4a9a62b83
tiara79 <loverjj2@naver.com>
2025-06-26 16:38:41 +0900

    error 404,500번 메세지 처리

--- 07/app.js
+++ 07/app.js
@@ -17,6 +17,23 @@ app.use("/notes", noteRouter);
 app.use("/todos", todoRouter);
 app.use("/posts", postRouter);
 
+// 모든 라우터 최하단에 404 처리 용도
+app.use((req,res) =>{
+  res.status(404).json({
+    status : "Fail",
+    message : "요청한 리소스는 찾을 수 없어요.",
+  });
+});
+
+// 500 의 경우에도 에러 처리
+app.use((err,req,res, )=>{
+   console.log(500).json({
+     status : "Error",
+     message: `server error : ${err.stack}`
+   })
+});
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`서버가 http://localhost:${PORT} 에서 실행 중 입니다. `);
