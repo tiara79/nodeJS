993c0044f6ec41411fbc528f35b05c2454ff89bf
tiara79 <loverjj2@naver.com>
2025-06-26 16:24:23 +0900

    댓글 수정/삭제

--- 07/routes/posts.js
+++ 07/routes/posts.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const postController = require("../controllers/posts");
-const { uploadSingle , uploadMultiple } = require("../middlewares/upload");
+const { uploadSingle, uploadMultiple } = require("../middlewares/upload");
 
 router.post("/", uploadMultiple, postController.createPost);
+router.get("/", postController.findPosts);
+router.get("/:id", postController.findPost);
+router.put("/:id", postController.updatePost);
+router.delete("/:id", postController.deletePost);
+
+router.post("/:postId/comments", postController.createComment);
+router.get("/:postId/comments", postController.findComments);
+router.put("/:postId/comments/:id", postController.updateComment);
+router.delete("/:postId/comments/:id", postController.deleteComment);
 
 module.exports = router;
