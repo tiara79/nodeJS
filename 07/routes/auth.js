56669b527cee3d2066d8c4d090e3679d7aca27df
tiara79 <loverjj2@naver.com>
2025-06-27 15:21:11 +0900

    라우터 validateRegister 연결

--- 07/routes/auth.js
+++ 07/routes/auth.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth");
+const { validateRegister} = require("../middlewares/validation")
 
-router.post("/register",authController.register);
+router.post("/register",validateRegister, authController.register);
 router.post("/login",authController.login);
 
 module.exports= router;
